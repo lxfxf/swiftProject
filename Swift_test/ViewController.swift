53e235260081e9894ce52806ed15ac70e96deee5
Liu Xuefeng <liu@dm5502.jaist.ac.jp>
2014-06-08 11:28:44 +0900

    Initial Commit

---
+++ Swift_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_test
+//
+//  Created by Liu Xuefeng on 6/8/14.
+//  Copyright (c) 2014 Liu Xuefeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
